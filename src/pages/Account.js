b4781197170d71d1665c2e8ee9748ae5956ed226
Toghrul Taghiyev <ttoghrul@gmail.com>
2022-09-13 00:08:47 +0100

    Added Profile page and personal details

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -10,6 +10,7 @@ import Iconify from '../components/Iconify';
 import {BlogPostCard, BlogPostsSort, BlogPostsSearch} from '../sections/@dashboard/blog';
 // mock
 import POSTS from '../_mock/blog';
+import AccountProfile from "./AccountProfile";
 
 // ----------------------------------------------------------------------
 
@@ -42,14 +43,16 @@ export default function Account() {
                 <Stack mb={5} direction="row" alignItems="center" justifyContent="space-between">
                     <Box sx={{width: '100%', typography: 'body1'}}>
                         <TabContext value={value}>
-                            <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
+                            <Box sx={{borderBottom: 1, borderColor: 'divider', spacing: 8}}>
                                 <TabList onChange={handleChange} aria-label="lab API tabs example">
-                                    <Tab label="Item One" value="1"/>
-                                    <Tab label="Item Two" value="2"/>
-                                    <Tab label="Item Three" value="3"/>
+                                    <Tab label="General" value="1"/>
+                                    <Tab label="Settings" value="2"/>
+                                    <Tab label="Change password" value="3"/>
                                 </TabList>
                             </Box>
-                            <TabPanel value="1">Item One</TabPanel>
+                            <TabPanel value="1">
+                                <AccountProfile/>
+                            </TabPanel>
                             <TabPanel value="2">Item Twos</TabPanel>
                             <TabPanel value="3">Item Three</TabPanel>
                         </TabContext>
---
+++ src/pages/AccountProfile.js
@@ -0,0 +1,155 @@
+import {useState} from "react";
+import {Link as RouterLink} from 'react-router-dom';
+import PropTypes from "prop-types";
+// material
+import {
+    Grid,
+    Button,
+    Container,
+    Stack,
+    Typography,
+    Box,
+    Tabs,
+    Tab,
+    Card,
+    Avatar,
+    TextField,
+    MenuItem
+} from '@mui/material';
+import {TabContext, TabList, TabPanel} from "@mui/lab";
+// components
+import Page from '../components/Page';
+import Iconify from '../components/Iconify';
+import {BlogPostCard, BlogPostsSort, BlogPostsSearch} from '../sections/@dashboard/blog';
+// mock
+import POSTS from '../_mock/blog';
+import {AppWidgetSummary} from "../sections/@dashboard/app";
+
+// ----------------------------------------------------------------------
+
+// TODO Remove unused imports and code functionality
+const SORT_OPTIONS = [
+    {value: 'latest', label: 'Latest'},
+    {value: 'popular', label: 'Popular'},
+    {value: 'oldest', label: 'Oldest'},
+];
+
+const countries =
+    [
+        {name: 'Afghanistan', code: 'AF'},
+        {name: 'Åland Islands', code: 'AX'},
+        {name: 'Albania', code: 'AL'},
+        {name: 'Algeria', code: 'DZ'},
+        {name: 'American Samoa', code: 'AS'},
+        {name: 'AndorrA', code: 'AD'},
+        {name: 'Angola', code: 'AO'},
+        {name: 'Anguilla', code: 'AI'},
+        {name: 'Antarctica', code: 'AQ'},
+        {name: 'Antigua and Barbuda', code: 'AG'},
+        {name: 'Argentina', code: 'AR'},
+        {name: 'Armenia', code: 'AM'},
+        {name: 'Aruba', code: 'AW'},
+        {name: 'Australia', code: 'AU'},
+        {name: 'Austria', code: 'AT'},
+        {name: 'Azerbaijan', code: 'AZ'},
+        {name: 'Bahamas', code: 'BS'},
+        {name: 'Bahrain', code: 'BH'},
+        {name: 'Bangladesh', code: 'BD'},
+        {name: 'Barbados', code: 'BB'},
+        {name: 'Belarus', code: 'BY'}
+    ]
+
+// ----------------------------------------------------------------------
+
+
+export default function AccountProfile() {
+
+    const [country, setCountry] = useState('Azerbaijan');
+
+    const handleChange = (event) => {
+        setCountry(event.target.value);
+    };
+
+    return (
+        <Page title="AccountProfile">
+            <Container>
+                <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{px: 1, py: 2}}>
+                    <Card sx={{width: 350, height: 500}}>
+                        <Box sx={{p: 15, pb: 1, borderRadius: '50%'}} dir="ltr">
+                            <Avatar alt="Remy Sharp" src="/static/mock-images/avatars/avatar_default.jpg"/>
+                        </Box>
+                    </Card>
+                    <Card sx={{width: 600, height: 500, pt: 2}}>
+                        <Box
+                            component="form"
+                            sx={{
+                                '& .MuiTextField-root': {m: 2, width: '25ch'},
+                            }}
+                            noValidate
+                            autoComplete="off"
+                        >
+                            <div>
+                                <TextField
+                                    id="outlined-required"
+                                    label="Name"
+                                    defaultValue="Toghrul Taghiyev"
+                                />
+                                <TextField
+                                    id="outlined-required"
+                                    label="Email Address"
+                                    defaultValue="ttoghrul@gmail.com"
+                                />
+                                <TextField
+                                    id="outlined-required"
+                                    label="Phone Number"
+                                    defaultValue="+994-706771237"
+                                />
+                                <TextField
+                                    id="outlined-required"
+                                    label="Address"
+                                    defaultValue="M.Mushvig street"
+                                />
+                                <TextField
+                                    id="outlined-required"
+                                    label="Country"
+                                    select
+                                    value={country}
+                                    onChange={handleChange}
+                                    defaultValue="Azerbaijan"
+                                >
+                                    {countries.map((option) => (
+                                        <MenuItem key={option.code} value={option.name}>
+                                            {option.name}
+                                        </MenuItem>
+                                    ))}
+                                </TextField>
+                                <TextField
+                                    id="outlined-required"
+                                    label="State/Region"
+                                    defaultValue="Absheron"
+                                />
+                                <TextField
+                                    id="outlined-required"
+                                    label="City"
+                                    defaultValue="Baku"
+                                />
+                                <TextField
+                                    id="outlined-required"
+                                    label="Zip/Code"
+                                    defaultValue="AZ 1073"
+                                />
+                                <TextField
+                                    sx={{width: 500}}
+                                    id="outlined-required"
+                                    label="About"
+                                    multiline
+                                    defaultValue="Nullam cursus lacinia erat. Vestibulum suscipit nulla quis orci. Fusce a quam."
+                                />
+                            </div>
+                        </Box>
+                    </Card>
+                </Stack>
+            </Container>
+        </Page>
+    );
+}
